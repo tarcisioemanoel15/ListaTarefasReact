b306e582142026243830adcb2b48851b5d87878c
tarcisio <tarcisioemanoel10031@gmail.com>
2022-01-20 19:44:58 -0300

    Curso-React

---
+++ src/components/Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>
+          { novaTarefa }
+
+        </h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
